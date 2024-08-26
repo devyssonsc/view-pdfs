be14868c8ae76d5980026096225f31892d2a3a84
Devysson Cardoso <devyssonsc@gmail.com>
2024-08-26 16:53:37 +0100

    create ViewComponent

---
+++ src/app/components/view-document/view-document.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { DocumentService } from '../../document.service';
+import { Subscription } from 'rxjs';
+import { Document } from '../../models/document.model';
+
+@Component({
+  selector: 'app-view-document',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-document.component.html',
+  styleUrl: './view-document.component.css'
+})
+export class ViewDocumentComponent {
+  nomeContratante: string = '';
+  nomeContratado: string = '';
+  valorContrato: number = 0;
+
+  constructor(private documentService: DocumentService){}
+
+  refresh() {
+    this.nomeContratante = this.documentService.document.getNomeContratante();
+    this.nomeContratado = this.documentService.document.getNomeContratado();
+    this.valorContrato = this.documentService.document.getValorContrato();
+  }
+}
