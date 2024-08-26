2435843fb717092942a8596de5ae34a34e486a78
Devysson Cardoso <devyssonsc@gmail.com>
2024-08-26 16:54:39 +0100

    Use UploadComponent and ViewComponent on AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormUploadComponent } from './components/form-upload/form-upload.component';
+import { UpdateDocumentComponent } from './components/update-document/update-document.component';
+import { ViewDocumentComponent } from './components/view-document/view-document.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormUploadComponent],
+  imports: [RouterOutlet, FormUploadComponent, UpdateDocumentComponent, ViewDocumentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
